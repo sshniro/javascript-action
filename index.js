d5bca2eda8651600e1d2026f19750eac3fb72120
sshniro <sshniro@gmail.com>
2020-03-06 15:49:56 +0100

    test

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
+  try {
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`)
 
@@ -13,7 +14,7 @@ async function run() {
     core.debug((new Date()).toTimeString())
 
     core.setOutput('time', new Date().toTimeString());
-  } 
+  }
   catch (error) {
     core.setFailed(error.message);
   }
